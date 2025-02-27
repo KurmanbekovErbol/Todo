8343397dbe441e74e9aa9dad6f8b1b1fa8fdef65
Erbol <kurmanbekov.erbol.07@gmail.com>
2025-02-27 16:21:29 +0600

    Моделки

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,14 +1,7 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
-class Users(models.Model):
-    username = models.CharField(
-        max_length=255,
-        verbose_name="Имя пользователя"
-    )
-    email = models.EmailField(
-        verbose_name="Адрес электронной почты"
-    )
+class Users(AbstractUser):
     phone_number = models.CharField(
         max_length=50,
         verbose_name='Номер телефона'
---
+++ apps/users/serializer.py
@@ -0,0 +1,42 @@
+from rest_framework import serializers
+from apps.users.models import Users
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Users
+        fields = ('id', 'username', 'email', 'phone_number', 'created_at', 'age', 'is_active')
+
+class RegisterUserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        max_length = 50,
+        write_only = True
+    )
+    confirm_password = serializers.CharField(
+        max_length = 50,
+        write_only = True
+    )
+    class Meta:
+        model = Users
+        fields = ('id', 'username', 'email', 'phone_number', 'created_at', 'age', 'is_active', 'password', 'confirm_password')
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['confirm_password']:
+            raise serializers.ValidationError({'confirm_password': 'Пароли отличаются'})
+        if len(attrs['password']) < 8:
+            raise serializers.ValidationError({"password": 'Пароль должен быть не менее 8 символов'})
+        if '+996' not in attrs['phone_number']:
+            raise serializers.ValidationError({'phone_number': 'Номер телефона должен быть в формате +996ХХХХХХХХХ'})
+        if len(attrs['phone_number']) < 13:
+            raise serializers.ValidationError({'phone_number': 'Некорректный номер телефона. Попробуйте ещё раз'})
+        return attrs
+    
+    def create(self, values):
+        user = Users.objects.create(
+            username=values['username'],
+            email=values['email'],
+            phone_number=values['phone_number'],
+            age=values['age']
+        )
+        user.set_password(values['password'])
+        user.save()
+        return user
