560752fbeca449498c5e5698c906d09a6a962b9e
Erbol <kurmanbekov.erbol.07@gmail.com>
2025-02-27 09:46:49 +0600

    Моделки

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    username = models.CharField(
+        max_length=255,
+        verbose_name="Имя пользователя"
+    )
+    email = models.EmailField(
+        verbose_name="Адрес электронной почты"
+    )
+    phone_number = models.CharField(
+        max_length=50,
+        verbose_name='Номер телефона'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Дата регистрации"
+    )
+    age = models.IntegerField(
+        verbose_name="Возраст"
+    )
+    def __str__(self):
+        return self.username
+    
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
