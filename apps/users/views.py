23d382b87b5630ce1a2450992f23802dc8576a8e
Erbol <kurmanbekov.erbol.07@gmail.com>
2025-02-27 16:27:27 +0600

    Views

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from apps.users.models import Users
+from apps.users.serializer import UserSerializer, RegisterUserSerializer
+
+class UserMixins(GenericViewSet,
+                 mixins.ListModelMixin,
+                 mixins.CreateModelMixin,
+                 mixins.DestroyModelMixin,
+                 mixins.RetrieveModelMixin):
+    queryset = Users.objects.all()
+    serializer_class = UserSerializer
+
+    def get_serializer_class(self):
+        if self.action in ('create', ):
+            return RegisterUserSerializer
+        return UserSerializer
