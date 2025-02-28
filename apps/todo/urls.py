a4f26557c8e18c5da6a2e37ca08e16aa942fd067
Erbol <kurmanbekov.erbol.07@gmail.com>
2025-02-28 17:41:25 +0600

    core.urls

--- apps/todo/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from apps.todo.views import TodoMixins
-
-router = DefaultRouter()
-
-router.register(r'todo', TodoMixins, basename='todos')
-
-urlpatterns = router.urls
--- core/urls.py
+++ core/urls.py
@@ -21,7 +21,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/todo/', include("apps.todo.urls")),
     path('api/users/', include("apps.users.urls"))
 ]
 
