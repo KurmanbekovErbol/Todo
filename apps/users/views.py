16874dd96d2d1e9a981f462b78faadf8393c97b7
Erbol <kurmanbekov.erbol.07@gmail.com>
2025-03-02 19:59:43 +0600

    Views

--- apps/users/views.py
+++ apps/users/views.py
@@ -3,6 +3,8 @@ from rest_framework.viewsets import GenericViewSet
 
 from apps.users.models import Users
 from apps.users.serializer import UserSerializer, RegisterUserSerializer
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
 class UserMixins(GenericViewSet,
                  mixins.ListModelMixin,
@@ -11,6 +13,11 @@ class UserMixins(GenericViewSet,
                  mixins.RetrieveModelMixin):
     queryset = Users.objects.all()
     serializer_class = UserSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+
+    search_fields = ['username']
+    ordering_fields = ['created_at']
+    ordering = ['-created_at']
 
     def get_serializer_class(self):
         if self.action in ('create', ):
