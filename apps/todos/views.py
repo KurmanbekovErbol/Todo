f874da5d71009c19ac792e003a6af5e0576303c9
Erbol <kurmanbekov.erbol.07@gmail.com>
2025-03-02 20:35:00 +0600

    view

--- apps/todos/views.py
+++ apps/todos/views.py
@@ -1,5 +1,7 @@
 from rest_framework import mixins
 from rest_framework.viewsets import GenericViewSet
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
 from apps.todos.models import Todo
 from apps.todos.serializer import TodoSerializer
@@ -13,3 +15,8 @@ class TodoMixins(GenericViewSet,
     
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+
+    search_fields = ['title', 'description']
+    ordering_fields = ['created_at']
+    ordering = ['-created_at']
