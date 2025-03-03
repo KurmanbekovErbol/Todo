21927eb781744032967bdf31b8e5767399dd38fe
Erbol <kurmanbekov.erbol.07@gmail.com>
2025-03-03 16:22:59 +0600

    Form

--- apps/todos/forms.py
+++ apps/todos/forms.py
@@ -2,7 +2,6 @@ from ckeditor.widgets import CKEditorWidget
 from django import forms
 from apps.todos.models import Todo
 
-
 class TodoForm(forms.ModelForm):
     description = forms.CharField(widget=CKEditorWidget())
 
